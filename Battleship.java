810554491b863cce27c92017720a9583e6f74c88
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-05-28 11:05:39 -0400

    inital gui

--- Battleship.java
+++ Battleship.java
@@ -1,5 +1,36 @@
-public class Battleship{
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Battleship extends JFrame implements ActionListener{
+    static final int SCREENWIDTH = 750;
+    static final int SCREENHEIGHT = 750;
+
+    public Battleship(){
+        setTitle("Battleship");
+        setSize(SCREENWIDTH, SCREENHEIGHT);
+
+        JPanel container = new JPanel(); //main panel
+        BoxLayout containerFrame = new BoxLayout(container, BoxLayout.X_AXIS);
+        container.setLayout(containerFrame);
+
+        Container contentPane = getContentPane(); //main container
+        contentPane.add(container);
+        setVisible(true);
+        setResizable(true);
+
+
+    }
     public static void main(String[]args){
-        System.out.println();
+        Battleship myBattleship = new Battleship();
+    }
+
+
+    public void actionPerformed(ActionEvent event){
+
     }
 }
